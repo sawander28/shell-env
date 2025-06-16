d6b645230a7795c75252c9b5a393629bd290e296
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-06-16 14:44:30 +0200

    updated gpg

--- .bashrc.d/30-gpg-agent.sh
+++ .bashrc.d/30-gpg-agent.sh
@@ -1,4 +1,10 @@
-#gpg-connect-agent reloadagent /bye >/dev/null 2>&1
+# gpg-conf --list-dirs
+#          --list-components
+#          --reload -R
+#          --kill -K
+#
+# gpg-connect-agent reloadagent /bye >/dev/null 2>&1
+#
 export GPG_TTY="$(tty)"
 export GPG_AGENT_INFO="$(gpgconf --list-dir agent-socket)"
 export SSH_AUTH_SOCK="$(gpgconf --list-dir agent-ssh-socket)"
