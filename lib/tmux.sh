93be4d2e19c763ebefa9e61118a43df6750a1d47
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-06-18 06:21:50 +0200

    tmux

---
+++ lib/tmux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+tmac () {
+    tmux has-session -t "$1" 2>/dev/null
+    if [ $? != 0 ]; then
+        tmux new-session -s "$1" -d
+    fi
+    tmux attach -t "$1"
+}
+
+_tmac_complete() {
+    local word=${COMP_WORDS[COMP_CWORD]}
+    local sessions=$(tmux list-sessions -F "#{session_name}" 2>/dev/null)
+    COMPREPLY=( $(compgen -W "$sessions" -- "$word") )
+}
+complete -F _tmac_complete tmac
