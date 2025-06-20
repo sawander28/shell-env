76684a84d61f5690233e740b56108db14a58ea2f
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-06-20 23:25:03 +0200

    rm perl

--- .profile.d/15-perl.sh
+++
@@ -1,7 +0,0 @@
-PERL5LIB="/home/wandsas/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"
-PERL_LOCAL_LIB_ROOT="/home/wandsas/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"
-PERL_MB_OPT="--install_base \"/home/wandsas/perl5\""
-PERL_MM_OPT="INSTALL_BASE=/home/wandsas/perl5"
-export PERL5LIB PERL_LOCAL_LIB_ROOT PERL_MB_OPT PERL_MM_OPT
-
-prepend_path ~/perl5/bin
