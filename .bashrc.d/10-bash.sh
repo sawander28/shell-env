7a0e62d244aea410e1bb2aa190ab08d4c29f3631
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-07-25 22:43:51 +0200

    clean

--- .bashrc.d/10-bash.sh
+++ .bashrc.d/10-bash.sh
@@ -20,3 +20,13 @@ man() {
   command man "$@"
 }
 # 
+man() {
+  LESS_TERMCAP_mb=$(printf "\e[01;33m") \
+  LESS_TERMCAP_md=$(printf "\e[01;33m") \
+  LESS_TERMCAP_me=$(printf "\e[0m") \
+  LESS_TERMCAP_se=$(printf "\e[0m") \
+  LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+  LESS_TERMCAP_ue=$(printf "\e[0m") \
+  LESS_TERMCAP_us=$(printf "\e[01;36m") \
+  command man "$@"
+}
--- .bashrc.d/20-direnv.sh
+++
@@ -1,6 +0,0 @@
-# Skip for noniteractive shells
-[ -z "$PS1" ] && return
-# Abort when command not exist
-command -v direnv >/dev/null 2>&1 || return
-
-eval "$(direnv hook bash)"
