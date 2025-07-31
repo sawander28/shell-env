24c4d08f5c79a8f5b1f76119a005178d1f2a2e19
Sascha Wander wandsas <sascha.wander@onionmail.com>
2025-07-31 16:56:49 +0200

    foo

--- .profile.d/30-go.sh
+++ .profile.d/30-go.sh
@@ -3,6 +3,5 @@ command -v go >/dev/null 2>&1 || test -d /usr/local/go || return
 
 export GOROOT=/usr/local/go
 export GOPATH=/home/wandsas/go
-
 prepend_path ${GOROOT}/bin
 prepend_path ${GOPATH}/bin
